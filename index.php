d8a9c8367d528b93577faebbbdaa2ecd427b5493
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-18 15:28:34 +0800

    set data for quiz

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 
+function h($s) {
+	return htmlspecialchars($s, ENT_QUOTES, 'utf-8');
+}
+
+$quizList = array(
+	array(
+		'q' => '最も可愛いのは？',
+		'a' => array('大島優子', '猫', 'Ryoma', '犬')
+	),
+	array(
+		'q' => '神に近いのは？',
+		'a' => array('大島優子', 'おとんのいびき', '蚊', '工事現場')
+	),
+	array(
+		'q' => '適度なボディは？',
+		'a' => array('大島優子', 'おかん', 'ともだち', 'サザエさん')
+	),
+	array(
+		'q' => '結婚するなら？',
+		'a' => array('大島優子', '美空ひばり', '蒼井そら', '尼さん')
+	)
+);
+
+var_dump($quizList);
+
 ?>
 
 <!DOCTYPE html>
